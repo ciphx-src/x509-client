8ddbab6d4a9f58c77e9143cc4199e22e77082e53
Curtis Lee Fulton <curtis@merlin.video>
2023-08-19 20:34:12 -0700

    default type for X509Iterator

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Debug, Display};
 
 /// X509 Deserializer API
-pub trait X509Iterator: IntoIterator
+pub trait X509Iterator<I = Self>: IntoIterator
 where
     Self: Sized,
 {
@@ -9,11 +9,11 @@ where
     type X509IteratorError: X509IteratorError;
 
     /// Attempt to deserialize, assume input is a single DER-encoded certificate
-    fn from_cer<T: AsRef<[u8]>>(src: T) -> Result<Self, Self::X509IteratorError>;
+    fn from_cer<T: AsRef<[u8]>>(src: T) -> Result<I, Self::X509IteratorError>;
     /// Attempt to deserialize, assume input is a stack of zero or more PEM-encoded certificates
-    fn from_pem<T: AsRef<[u8]>>(src: T) -> Result<Self, Self::X509IteratorError>;
+    fn from_pem<T: AsRef<[u8]>>(src: T) -> Result<I, Self::X509IteratorError>;
     /// Attempt to deserialize, assume input is a DER-encoded PKCS7 certificate bundle
-    fn from_pkcs7<T: AsRef<[u8]>>(src: T) -> Result<Self, Self::X509IteratorError>;
+    fn from_pkcs7<T: AsRef<[u8]>>(src: T) -> Result<I, Self::X509IteratorError>;
 }
 
 /// Error type bounds
