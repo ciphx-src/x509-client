81ec43958599ea9ea46b9307ddc1b6b590003bb8
Curtis Lee Fulton <curtis@merlin.video>
2023-08-22 10:37:16 -0700

    tests

--- src/provided/debug.rs
+++ src/provided/debug.rs
@@ -1,34 +1,35 @@
 use crate::api::X509Iterator;
 use bytes::Bytes;
 use std::convert::Infallible;
-use std::iter;
+use std::vec;
 
 #[derive(Clone, Debug)]
-pub struct DebugX509Iterator(Bytes);
+pub struct DebugX509Iterator(Option<Bytes>);
 
 impl From<Bytes> for DebugX509Iterator {
     fn from(bytes: Bytes) -> Self {
-        Self(bytes)
+        Self(Some(bytes))
     }
 }
 
 impl From<DebugX509Iterator> for Bytes {
     fn from(src: DebugX509Iterator) -> Self {
-        src.0
+        src.0.unwrap_or_else(Bytes::new)
     }
 }
 impl IntoIterator for DebugX509Iterator {
     type Item = Bytes;
-    type IntoIter = std::iter::Once<Self::Item>;
+    type IntoIter = vec::IntoIter<Self::Item>;
 
     fn into_iter(self) -> Self::IntoIter {
-        iter::once(self.0)
+        self.0
+            .map_or_else(|| vec![].into_iter(), |i| vec![i].into_iter())
     }
 }
 
 impl FromIterator<<Self as IntoIterator>::Item> for DebugX509Iterator {
     fn from_iter<T: IntoIterator<Item = <Self as IntoIterator>::Item>>(iter: T) -> Self {
-        Self(iter.into_iter().next().unwrap_or(Bytes::new()))
+        Self(iter.into_iter().next())
     }
 }
 
@@ -36,14 +37,14 @@ impl X509Iterator for DebugX509Iterator {
     type X509IteratorError = Infallible;
 
     fn from_cer<T: AsRef<[u8]>>(src: T) -> Result<Self, Self::X509IteratorError> {
-        Ok(Self(Bytes::copy_from_slice(src.as_ref())))
+        Ok(Self(Some(Bytes::copy_from_slice(src.as_ref()))))
     }
 
     fn from_pem<T: AsRef<[u8]>>(src: T) -> Result<Self, Self::X509IteratorError> {
-        Ok(Self(Bytes::copy_from_slice(src.as_ref())))
+        Ok(Self(Some(Bytes::copy_from_slice(src.as_ref()))))
     }
 
     fn from_pkcs7<T: AsRef<[u8]>>(src: T) -> Result<Self, Self::X509IteratorError> {
-        Ok(Self(Bytes::copy_from_slice(src.as_ref())))
+        Ok(Self(Some(Bytes::copy_from_slice(src.as_ref()))))
     }
 }
